9800e69606f5b5faacca714a9203599c2d82b65c
Ivornikk <metodievi038@gmail.com>
2025-10-03 13:46:14 +0300

    Fix opensearch client address

--- server/openSearch.mjs
+++ server/openSearch.mjs
@@ -1,3 +1,3 @@
 import { Client } from "@opensearch-project/opensearch";
 
-export const osClient = new Client({ node: 'http://localhost:9200' })
+export const osClient = new Client({ node: 'http://opensearch:9200' })
