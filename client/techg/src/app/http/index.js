be46193ee09132b35ce50ded4c85048a166f2266
Ivornikk <metodievi038@gmail.com>
2025-10-06 22:00:35 +0300

    Fix conflict with production and development API URL's

--- client/techg/src/app/http/index.js
+++ client/techg/src/app/http/index.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
+
+let baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+// Removes the '/api' from the end of URL
+baseUrl = baseUrl.slice(0, -4)
+
 const $host = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseURL: baseUrl,
     withCredentials: true
 })
 
 const $authHost = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseURL: baseUrl,
     withCredentials: true
 })
 
