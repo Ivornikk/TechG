818b4783e12cec1c396acabba729a426bba7d044
Ivornikk <metodievi038@gmail.com>
2025-10-07 05:50:14 +0300

    Make changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
+console.log("DIRNAME: ", path.resolve(__dirname, 'static'), "wafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwagwafwadfwgwafawawgwgwgwgawgwagwggwagwag")
 const App = express()
 
 App.use(cors({
