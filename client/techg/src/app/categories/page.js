b05fa6b979e12f2b9c4642fea5b6defac1c81f1e
Ivornikk <metodievi038@gmail.com>
2025-10-10 01:57:16 +0300

    Create categories page (Close #37)

--- client/techg/src/app/categories/page.js
+++ client/techg/src/app/categories/page.js
@@ -4,10 +4,12 @@ import { useContext, useEffect, useState } from "react"
 import { fetchCategories } from "../http/ProductAPI"
 import { StoreContext } from "../store/StoreProvider"
 import { observer } from "mobx-react-lite"
+import Link from "next/link"
 
 const Categories = observer(() => {
     const { product } = useContext(StoreContext)
     const [selectedCategory, setSelectedCategory] = useState(0)
+    const [selectedType, setSelectedType] = useState(0)
     const [isMobile, setIsMobile] = useState(false)
 
     useEffect(() => {
@@ -44,7 +46,7 @@ const Categories = observer(() => {
     return (
         <div className="md:m-10">
             { isMobile ?
-            <div>
+            <div className="flex">
                 <aside className="min-h-[100vh] bg-categories w-25">
                     <ul className="flex flex-col items-center">
                         { product.categoriesObject.rows?.map(category => {
@@ -63,13 +65,37 @@ const Categories = observer(() => {
                         })}
                     </ul>
                 </aside>
-                <div>
+                <div className="m-3 w-full">
                     <ul>
                         {console.log(getCategoryId(selectedCategory))}
                         { getCategoryId(selectedCategory)?.types.map(type => {
                             return (
-                                <li key={type.id}>
-                                    {type.name}
+                                <li key={type.id}
+                                    className="py-3 flex flex-col"
+                                    onClick={() => setSelectedType(type.id)}>
+                                    <div className="w-full flex justify-between">
+                                        {type.name}
+                                        <img src="/dropdown-arrow.svg"
+                                            className={`invert mx-5 -rotate-90 transition-all ${
+                                                type.id == selectedType && 'rotate-0'
+                                            }`}>
+                                        </img>
+                                    </div>
+                                    { selectedType == type.id &&
+                                        <ul className="grid grid-cols-2 gap-3 mt-3">
+                                            { type.groups.map(group => {
+                                                return (
+                                                    <li key={group.id}
+                                                        className="h-12 border border-brand rounded-xl flex items-center justify-center">
+                                                        <Link href={`/category/${group.id}`}
+                                                            className=" text-center">
+                                                            {group.name}
+                                                        </Link>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    }
                                 </li>
                             )
                         })}
@@ -77,8 +103,29 @@ const Categories = observer(() => {
                 </div>
             </div>
             :
-            <div>
-
+            <div className="xl:max-w-[60vw] max-w-[70vw] m-auto min-h-[100vh]">
+                <ul className="grid xl:grid-cols-3 grid-cols-2 gap-10">
+                    { product.categoriesObject.rows.map(category => {
+                        return(
+                            <li key={category.id} 
+                                className="bg-categories flex flex-col items-center px-5">
+                                <h1 className="text-[1.4em] mb-5">
+                                    {category.name}
+                                </h1>
+                                <ul className="text-[1.2em] flex flex-col gap-4 items-start w-full">
+                                    { category.types.map(type => {
+                                        return (
+                                            <li key={type.id}
+                                                className="cursor-pointer hover:underline hover:text-brand">
+                                                {type.name}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
             }
         </div>
